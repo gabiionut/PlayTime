f0ead28853bb362ca900225748633be118a25e94
Ionut Gabriel Pitigoi <pitigoiionutgabriel@gmail.com>
2020-03-29 16:59:21 +0300

    Fix for iOS

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/user/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/user/Downloads/Developer/AndroidX/are_we_there_yet"
-export "FLUTTER_TARGET=/Users/user/Downloads/Developer/AndroidX/are_we_there_yet/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/user/Developer/flutter/PlayTime"
+export "FLUTTER_TARGET=/Users/user/Developer/flutter/PlayTime/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/user/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.2"
-export "FLUTTER_BUILD_NUMBER=8"
+export "FLUTTER_BUILD_NUMBER=9"
 export "TRACK_WIDGET_CREATION=true"
